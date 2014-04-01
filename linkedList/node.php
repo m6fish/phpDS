608f6bf45180565342f78f72998ee19ce4964164
Rfish <haopingyu@kkbox.com>
2014-04-01 18:15:23 +0800

    node constructor

--- linkedList/node.php
+++ linkedList/node.php
@@ -2,13 +2,24 @@
 
 class node {
     
-    $next;
     $prev;
     $data;
+    $next;
 
-    function __construct(){
-        
-        
+    public function __construct(){
+        $argNum = func_num_args();
+        $argArr = func_get_args();
+
+        if(1 == $argNum ){
+            $this->data = $argArr[0];
+        } else if (3 == $argNum ){
+            $this->prev = $argArr[0];
+            $this->data = $argArr[1];
+            $this->next = $argArr[2];
+        } else {
+            return "wrong arg nums!\n";
+        }
+
     }
 
 
