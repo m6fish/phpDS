e52dfa057b6a81380ad830fc20f3e73716430090
Rfish <haopingyu@kkbox.com>
2014-04-22 16:19:38 +0800

    linkedList - modify demo data

--- linkedList/demo.php
+++ linkedList/demo.php
@@ -5,6 +5,24 @@ $myList = new myLinkedList();
 
 $myList->printList();
 
+$myList->addFirst("apple");
+$myList->printList();
+
+$myList->addLast("cat");
+$myList->printList();
+
+$myList->insert(1,"banana");
+$myList->printList();
 
 
+$myList->insert(1,"GMO");
+$myList->insert(2,"KKBOX");
+$myList->addFirst("DUTY");
+$myList->addLast("TIME");
+$myList->insert(3,"FUTURE");
+$myList->printList();
+
+
+$myList->remove("apple");
+$myList->printList();
 
